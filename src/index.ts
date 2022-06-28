1f6eefac3d2fb6c93aecaee19b4e6f15c1e67c18
jubin369 <jubinayoob369@gmail.com>
2022-06-28 14:54:31 +0530

    error in mikroorm

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,13 @@ import mikroConfig from "./mikro-orm.config";
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
 
-  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: "my first post" });
 
-  await orm.em.persistAndFlush(post);
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch((err) => {
